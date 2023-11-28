fdb22337b32565ec51093e0208884845ff121449
Tetiana Fesenko <Fesenko.tania@gmail.com>
2023-11-28 18:51:20 -0500

    quiz data

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -5,6 +5,8 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.appcompat.app.AlertDialog;
+
 
 public class MainActivity extends AppCompatActivity implements QuizFragment.OnAnswerSelectedListener {
 
@@ -67,4 +69,43 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
         findViewById(R.id.fragment_container).setBackgroundColor(backgroundColor);
     }
 
+    private void showResultAlertDialog(int correctCount) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Quiz Finished");
+        builder.setMessage("You answered " + correctCount + " questions correctly. Do you want to save the result?");
+
+        builder.setPositiveButton("Save", (dialog, which) -> {
+            // Save the result (number of correct questions) to the file system
+            saveResult(correctCount);
+
+            // Reset the quiz for another attempt
+            resetQuiz();
+        });
+
+        builder.setNegativeButton("Ignore", (dialog, which) -> {
+            // Reset the quiz for another attempt without saving
+            resetQuiz();
+        });
+
+        builder.show();
+    }
+    // Define the resetQuiz method
+    private void resetQuiz() {
+        currentQuestionIndex = 0;
+        // You may need additional logic to reset other quiz-related variables or UI elements
+        showQuizFragment(); // To show the first question again
+    }
+    private void saveResult(int correctCount) {
+        // Create a file to save the result
+        String fileName = "quiz_result.txt";
+        try (FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE)) {
+            // Write the correct count to the file
+            String resultString = String.valueOf(correctCount);
+            fos.write(resultString.getBytes());
+            Toast.makeText(this, "Result saved!", Toast.LENGTH_SHORT).show();
+        } catch (IOException e) {
+            e.printStackTrace();
+            Toast.makeText(this, "Error saving result", Toast.LENGTH_SHORT).show();
+        }
+    }
 }
