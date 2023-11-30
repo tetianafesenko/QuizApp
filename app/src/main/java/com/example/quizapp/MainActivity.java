5b0d98bfbaaf85b40b8bed4b6ebd9a1284095793
Tetiana Fesenko <Fesenko.tania@gmail.com>
2023-11-30 12:17:16 -0500

    menu updates

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.quizapp;
 
+
 import android.content.Context;
 import android.os.Bundle;
-import android.widget.ProgressBar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +16,7 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
 
     private int currentQuestionIndex = 0;
     private int correctAnswerCount = 0;
+    private int totalAttempts = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,8 +26,32 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
         showQuizFragment();
     }
 
-    private void showQuizFragment() {
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int itemId = item.getItemId();
+
+        if (itemId == R.id.menu_get_average) {
+            showAverageReport();
+            return true;
+        } else if (itemId == R.id.menu_select_questions) {
+            showSelectQuestionsDialog();
+            return true;
+        } else if (itemId == R.id.menu_reset_results) {
+            showResetResultsDialog();
+            return true;
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+
+    private void showQuizFragment() {
         FragmentTransaction ft = getSupportFragmentManager().beginTransaction();
         ft.replace(R.id.fragment_container, QuizFragment.newInstance(currentQuestionIndex));
         ft.commit();
@@ -39,19 +66,17 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
         }
 
         currentQuestionIndex++;
+        totalAttempts++;
 
         if (currentQuestionIndex < QuizData.questions.size()) {
             showQuizFragment();
         } else {
-            // The quiz is finished
             showQuizCompletionMessage();
-            showResultAlertDialog(correctAnswerCount); // Show the result dialog
+            showResultAlertDialog(correctAnswerCount);
         }
     }
 
-
     private void showQuizCompletionMessage() {
-        // Display that the quiz is completed
         Toast.makeText(this, "Quiz Completed!", Toast.LENGTH_SHORT).show();
     }
 
@@ -59,21 +84,66 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
         String message = isCorrect ? "Correct!" : "Incorrect!";
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
     }
+
+    private void showAverageReport() {
+        if (totalAttempts > 0) {
+            float average = (float) correctAnswerCount / totalAttempts;
+            String averageMessage = "Average: " + average;
+            showToast(averageMessage);
+        } else {
+            showToast("No attempts yet");
+        }
+    }
+
+    private void showSelectQuestionsDialog() {
+        // Placeholder for the dialog to select the number of questions
+        // You can replace this with the actual logic for your dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Select Number of Questions");
+        builder.setMessage("Implement your logic here");
+        builder.setPositiveButton("OK", (dialog, which) -> {
+            // Handle the selection
+            showToast("Dialog will handle selection");
+        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> {
+            // Handle cancellation
+            showToast("Dialog canceled");
+        });
+        builder.show();
+    }
+
+    private void showResetResultsDialog() {
+        // Placeholder for the dialog to reset saved results
+        // You can replace this with the actual logic for your dialog
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Reset Saved Results");
+        builder.setMessage("Are you sure you want to reset saved results?");
+        builder.setPositiveButton("Yes", (dialog, which) -> {
+            // Reset saved results logic
+            showToast("Results reset");
+        });
+        builder.setNegativeButton("No", (dialog, which) -> {
+            // Handle cancellation
+            showToast("Reset canceled");
+        });
+        builder.show();
+    }
+
+    private void showToast(String message) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+    }
+
     private void showResultAlertDialog(int correctCount) {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Quiz Finished");
         builder.setMessage("You answered " + correctCount + " questions correctly. Do you want to save the result?");
 
         builder.setPositiveButton("Save", (dialog, which) -> {
-            // Save the result
             saveResult(correctCount);
-
-            // Reset the quiz
             resetQuiz();
         });
 
         builder.setNegativeButton("Ignore", (dialog, which) -> {
-
             resetQuiz();
         });
 
@@ -83,14 +153,12 @@ public class MainActivity extends AppCompatActivity implements QuizFragment.OnAn
     private void resetQuiz() {
         currentQuestionIndex = 0;
         correctAnswerCount = 0;
-        showQuizFragment(); // To show the first question again
+        showQuizFragment();
     }
 
     private void saveResult(int correctCount) {
-        // Create a file to save the result
         String fileName = "quiz_result.txt";
         try (FileOutputStream fos = openFileOutput(fileName, Context.MODE_PRIVATE)) {
-
             String resultString = String.valueOf(correctCount);
             fos.write(resultString.getBytes());
             Toast.makeText(this, "Result saved!", Toast.LENGTH_SHORT).show();
